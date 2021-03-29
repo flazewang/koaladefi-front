4c3dfe2f1e8e0a7d987d2d83a1621a614dfd24d5
flazewang <71084388+flazewang@users.noreply.github.com>
2021-03-29 23:58:25 +0800

    Update useFarmsWithBalance.ts

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingLyptus',
+        name: 'pendingPotato',
         params: [farm.pid, account],
       }))
 
