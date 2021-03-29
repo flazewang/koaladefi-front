62753eb156dbc47cd01c16de1901e7d753ff2dd2
flazewang <71084388+flazewang@users.noreply.github.com>
2021-03-29 23:57:54 +0800

    Update useAllEarnings.ts

--- src/hooks/useAllEarnings.ts
+++ src/hooks/useAllEarnings.ts
@@ -15,7 +15,7 @@ const useAllEarnings = () => {
     const fetchAllBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingLyptus',
+        name: 'pendingPotato',
         params: [farm.pid, account],
       }))
 
