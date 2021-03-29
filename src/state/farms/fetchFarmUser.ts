a7f3c1a5b0bf9299455dbd130beed2100e475377
flazewang <71084388+flazewang@users.noreply.github.com>
2021-03-29 23:59:44 +0800

    Update fetchFarmUser.ts

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -63,7 +63,7 @@ export const fetchFarmUserEarnings = async (account: string) => {
   const calls = farmsConfig.map((farm) => {
     return {
       address: masterChefAdress,
-      name: 'pendingLyptus',
+      name: 'pendingPotato',
       params: [farm.pid, account],
     }
   })
